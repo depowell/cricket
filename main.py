fd5c3a0d43ca453baa04e4b741ccb1889603355d
David Powell <david.powell@outlook.com>
2018-07-04 07:59:27 +1200

    logic changes

--- main.py
+++ main.py
@@ -8,15 +8,13 @@ def bowling():
     runs = 0
     while True:
         run = random.choice(posruns)
-        guess = int(raw_input("Cricket! You are bowling, guess the number of runs: "))
         while True:
-            if guess in posruns:
+            guess = raw_input("Cricket! You are bowling, guess the number of runs: ")
+            if guess != "" and int(guess) in posruns:
                 break
             else:
                 print("Please guess a valid Cricket score in this list: (0,1,2,3,4,6)")
-                guess = int(raw_input("Cricket! You are bowling, guess the number of runs: "))
-
-        if run == guess:
+        if run == int(guess):
             print("Computer scored! " + str(run) + " runs, you guessed " + str(guess))
             print("Computer is out! You guessed correctly")
             print("The score was " + str(runs) + " runs")
@@ -29,21 +27,20 @@ def batting():
     runs = 0
     while True: 
         guess = random.choice(posruns)
-        run = int(raw_input("You are batting, how many runs do you hit: "))
         while True:
-            if run in posruns:
+            run = raw_input("You are batting, how many runs do you hit: ")
+            if run != "" and  int(run) in posruns:
                 break
             else:
                 print("Please guess a valid Cricket score in this list: (0,1,2,3,4,6)")
-                run = int(raw_input("You are batting, how many runs do you hit: "))
-        if run == guess:
+        if int(run) == guess:
             print("You scored! " + str(run) + " runs, computer guessed " + str(guess))
             print("Player is out! Computer guessed your runs")
             print("The score was " + str(runs) + " runs")
             break
         else:
             print("You scored! " + str(run) + " runs, computer guessed " + str(guess))
-            runs += run
+            runs += int(run)
 
 if reply[:1] == 'y':
     print("You chose to Bat, enter your number of runs: ")
